21ac10c3495f271967b0dce54d001af682209023
Wesley Gurgel <wesleygurgel27@gmail.com>
2021-07-04 12:50:35 -0300

    Criando tabela de Eventos para trabalhar com agenda

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
 
 # Create your views here.
+
+def soma(request, numero_a, numero_b):
+    total = numero_a + numero_b
+    return HttpResponse('<h1>A soma é {}</h1>'.format(total))
--- django_dio/urls.py
+++ django_dio/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('soma/<int:numero_a>/<int:numero_b>/', views.soma)
 ]
